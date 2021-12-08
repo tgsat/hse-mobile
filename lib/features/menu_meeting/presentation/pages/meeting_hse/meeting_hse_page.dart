4200e67d06d808828452042fde26a9c2dc80ad23
satriopragma <satrio.pragma@gmail.com>
2021-12-08 11:26:34 +0700

    ,

---
+++ lib/features/menu_meeting/presentation/pages/meeting_hse/meeting_hse_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:hse_product/core/utils/values/size_config.dart';
+import 'package:hse_product/features/menu_meeting/presentation/pages/meeting_detail_page.dart';
+import 'package:hse_product/features/menu_meeting/presentation/widgets/item_data.dart';
+import 'package:hse_product/features/menu_meeting/presentation/widgets/searchBar_meeting.dart';
+
+class MeetingHSEPage extends StatelessWidget {
+  const MeetingHSEPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+          vertical: SizeConfig.marginActivity,
+          horizontal: SizeConfig.marginActivity),
+      child: Column(
+        children: [
+          SearchBarMeeting(),
+          SizedBox(height: 10),
+          Divider(color: Colors.grey.shade100),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MenuMeetingDetailPage()));
+                      },
+                      child: ItemDataMenuMeeting(),
+                    );
+                  }))
+        ],
+      ),
+    );
+  }
+}
--- lib/features/menu_meeting/presentation/pages/meeting_page.dart
+++ lib/features/menu_meeting/presentation/pages/meeting_page.dart
@@ -1,43 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:hse_product/core/presentation/widgets/app_bars/app_bar_white.dart';
 import 'package:hse_product/core/utils/translations/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:hse_product/core/utils/values/size_config.dart';
-import 'package:hse_product/features/menu_meeting/presentation/pages/meeting_detail_page.dart';
-import 'package:hse_product/features/menu_meeting/presentation/widgets/item_data.dart';
+import 'package:hse_product/core/utils/values/color_config.dart';
+import 'package:hse_product/features/menu_meeting/presentation/pages/meeting_hse/meeting_hse_page.dart';
+import 'package:hse_product/features/menu_meeting/presentation/pages/meeting_p2k3/meeting_p2k3_page.dart';
 
 class MenuMeetingPage extends StatelessWidget {
   const MenuMeetingPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBarWhite(LocaleKeys.meeting.tr(), context),
-      body: Container(
-        margin: EdgeInsets.symmetric(
-            vertical: SizeConfig.marginActivity,
-            horizontal: SizeConfig.marginActivity),
-        child: Column(
-          children: [
-            SizedBox(height: 10.0),
-            Divider(color: Colors.grey.shade100),
-            Expanded(
-                child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MenuMeetingDetailPage()));
-                  },
-                  child: ItemDataMenuMeeting(),
-                );
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            title: Text(
+              LocaleKeys.meeting.tr(),
+              style: Theme.of(context)
+                  .textTheme
+                  .headline6!
+                  .apply(color: Colors.black),
+            ),
+            iconTheme: IconThemeData(
+              color: Colors.black, //change your color here
+            ),
+            leading: InkWell(
+              onTap: () {
+                Navigator.of(context).pop();
               },
-            ))
-          ],
-        ),
-      ),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: ColorConfig.primaryColor,
+                    child: Center(
+                      child: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                      ),
+                    )),
+              ),
+            ),
+            bottom: TabBar(
+              indicatorColor: ColorConfig.primaryColor,
+              tabs: [
+                Tab(icon: itemText(LocaleKeys.meeting.tr() + " HSE", context)),
+                Tab(icon: itemText(LocaleKeys.meeting.tr() + " P2K3", context)),
+              ],
+            ),
+          ),
+          body: TabBarView(children: [MeetingHSEPage(), MeetingP2k3Page()]),
+        ));
+  }
+
+  itemText(String text, BuildContext context) {
+    return Text(
+      text,
+      style: Theme.of(context)
+          .textTheme
+          .subtitle2!
+          .apply(color: ColorConfig.primaryColor),
     );
   }
 }
